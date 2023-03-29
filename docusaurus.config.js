dea3b39f5989752bff72087acb07b8790e7480b7
Gabriel Vidal-Ayrinhac <gvidalayrinhac@gmail.com>
2023-03-29 08:59:50 +0200

    add tailwindcss

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -160,6 +160,17 @@ async function createConfig() {
             // ... other options
           },
         ],
+        async function myPlugin(context, options) {
+          return {
+            name: "docusaurus-tailwindcss",
+            configurePostCss(postcssOptions) {
+              // Appends TailwindCSS and AutoPrefixer.
+              postcssOptions.plugins.push(require("tailwindcss"));
+              postcssOptions.plugins.push(require("autoprefixer"));
+              return postcssOptions;
+            },
+          };
+        },      
       ],    
   };
 }
